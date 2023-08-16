80b1471d5f595405a7a7b8f277c5b26307a94cf9
Vladislav Razoronov <razoronov@ucu.edu.ua>
2023-08-16 17:50:18 +0300

    upload_task1

---
+++ lesson1/main.js
@@ -0,0 +1,34 @@
+let one ='hello';
+let two ='owu';
+let three ='com';
+let four ='ua';
+let five =1;
+let six =10;
+let seven =-999;
+let eight =123;
+let pi =3.14;
+let nine = 2.7;
+let ten = 16;
+let eleven = true;
+let twelve = false;
+console.log(one, two, three, four, five, six, seven, eight, pi, nine, ten, eleven, twelve );
+
+let fName = "Владислав";
+let mName = "Романович";
+let lName = "Разорьонов";
+let person = fName +" "+lName+" "+mName;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let name1 = prompt("Введіть ім'я");
+let name2 = prompt("Введіть прізвище");
+let name3 = prompt("Введіть по-батькові");
+
+console.log(`${name1} ${name2} ${name3}`);
