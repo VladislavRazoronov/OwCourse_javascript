589ffe689b923e85004a4574df115b799c1e2949
Vladislav Razoronov <razoronov@ucu.edu.ua>
2023-08-30 14:19:28 +0300

    upload_task5

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,24 +1,27 @@
+//ВСЕ ЗРОБИТИ СТРІЛОЧНИМИ ФУНКЦІЯМИ!
 //- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-function area(a,b){
+// noinspection DuplicatedCode
+
+let area = (a,b) =>{
     return a*b;
 }
 
 console.log(area(5,10));
 
 //- створити функцію яка обчислює та повертає площу кола з радіусом r
-function circleArea(r){
+let circleArea = (r) =>{
     return 3.14*r^2;
 }
 console.log(circleArea(12));
 
 //- створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-function cylinderArea(h,r){
+let cylinderArea = (h,r)=>{
     return 2*3.14*h*r+2*circleArea(r);
 }
 console.log(cylinderArea(4,2));
 
 //- створити функцію яка приймає масив та виводить кожен його елемент
-function printAll(arr){
+let printAll = (arr)=>{
     if(arr) {
         for (let i = 0; i < arr.length; i++) {
             console.log(arr[i]);
@@ -28,13 +31,13 @@ function printAll(arr){
 printAll(["a","C",2343,"DC",true]);
 
 //- створити функцію яка створює параграф з текстом. Текст задати через аргумент
-function createParagraph(text){
+let createParagraph = (text)=>{
     document.write(`<p>${text}</p>`);
 }
 createParagraph("Big Circle");
 
 //- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function createList(text){
+let createList =(text)=>{
     document.write(`<ul>
     <li>${text}</li>
     <li>${text}</li>
@@ -44,7 +47,7 @@ function createList(text){
 createList("List Item");
 
 //- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function createAdjustableList(text,count){
+let createAdjustableList =(text,count)=>{
     if(!count || count<1){
         console.log("Invalid count");
         return;
@@ -58,7 +61,7 @@ function createAdjustableList(text,count){
 createAdjustableList("AdjustableListItem",6);
 
 //- створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-function createArray(array){
+let createArray =(array)=>{
     document.write("<ul>")
     for( let i=0; i<array.length;i++){
         document.write(`<li>${array[i]}</li>`)
@@ -68,7 +71,7 @@ function createArray(array){
 createArray(["nho",24321,4.532,true,"no",false]);
 
 //- створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-function addObjects(objects){
+let addObjects =(objects)=>{
     for(let obj of objects){
         document.write(`<div>
         <p>Name:${obj.name}</p>
@@ -81,7 +84,7 @@ let obj=[{id:2332,name:"item1",age:2},{id:3902,name:"item2",age:5},{id:7234,name
 addObjects(obj);
 
 //- створити функцію яка повертає найменьше число з масиву
-function getMin(numbers){
+let getMin =(numbers)=>{
     let min = NaN;
     for(let num of numbers){
         num = parseInt(num);
@@ -94,7 +97,7 @@ function getMin(numbers){
 console.log(getMin([232,21,1,90,0]));
 
 //- створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-function arraySum(numbers){
+let arraySum =(numbers)=>{
     let result=0;
     for(let num of numbers){
         result += parseInt(num);
@@ -105,7 +108,7 @@ console.log(arraySum([23,77,50,200]));
 
 //- створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 //Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-function swap(arr,index1,index2){
+let swap =(arr,index1,index2)=>{
     if(!arr || typeof index1 != "number" || typeof index2 != "number"){
         console.log("Invalid function input");
         return;
@@ -121,7 +124,7 @@ console.log(swap(arr1,0,3));
 
 //- Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 //Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
-function exchange(sumUAH,currencyValue, exchangeCurrency){
+let exchange =(sumUAH,currencyValue, exchangeCurrency)=>{
     let selectedCurrency = "Err";
     let rate = 0;
     for(let curr of currencyValue){
